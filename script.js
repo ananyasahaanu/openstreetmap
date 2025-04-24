e200b03451371a5ec189c2c7324542c45b62c919
Ananya Saha <ananyasaha462@gmail.com>
2025-04-24 22:27:15 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showTab(id) {
+  const tabs = document.querySelectorAll(".tab");
+  tabs.forEach(tab => tab.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
+}
